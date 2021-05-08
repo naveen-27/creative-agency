71a2aed88da9036457a6556e40c3257d951e739e
Naveen Kumar <456endson@gmail.com>
2021-05-08 19:12:42 +0530

    Included Hero component inside App to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App: FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
 
 export default App;
