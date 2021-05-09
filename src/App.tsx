20eeb7ea83929f1bf28046966b4979ea89f98182
Naveen Kumar <456endson@gmail.com>
2021-05-09 19:24:48 +0530

    App renders Design component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { FC } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Design from "./components/Design";
 
 const App: FC = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Design />
       <Footer />
     </>
   );
