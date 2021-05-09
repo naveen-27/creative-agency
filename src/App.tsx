64505278ca09da2c40e268f3c58edade4c8678ab
Naveen Kumar <456endson@gmail.com>
2021-05-09 13:35:15 +0530

    Rendered Footer in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 const App: FC = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   );
 };
